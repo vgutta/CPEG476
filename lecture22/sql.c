fde2a7feb316ccc91a89ed43ea750c6ce0255ffa
vgutta <vineethg@udel.edu>
2018-10-29 17:20:44 +0000

    lecture22

---
+++ lecture22/sql.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sqlite3.h>
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+    int i;
+    for(i=0; i<argc; i++){
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+int main(){
+    sqlite3* db;
+    char *zErrMsg = 0;
+    int rc;
+    
+    rc = sqlite3_open("mail.db", &db);
+    if( rc ){
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    };
+    
+    char* sql = "CREATE TABLE USERS( ID INT PRIMARY KEY, NAME  TEXT );";
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    if( rc != SQLITE_OK ){
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+    }else{
+      fprintf(stdout, "Table created successfully\n");
+    }    
+    sqlite3_close(db);
+    return 0;
+};
